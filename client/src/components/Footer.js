0c3a8966deffdd10a7e4740aabdefc85d220e9e9
Yazid <yalhujery01@gmail.com>
2022-05-16 18:24:17 -0400

    final project

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,13 +5,13 @@ function Footer() {
         <footer class="text-center text-white" style={{
             backgroundColor: "#f1f1f1"
         }}>
-            <div class="container pt-4">
+            {/* <div class="container pt-4">
                 <section class="mb-4">
                     <a class="btn btn-floating m-1"
                         href="https://www.linkedin.com/in/yazid-alhujery/" role="button">
                             <img id="logo" src="./assets/linkedin.png" alt="logo" /></a>
                 </section>
-            </div>
+            </div> */}
         </footer>
     )
 }
